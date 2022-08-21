6b4195d0e4354ea8eec11f13566df64befd76b6d
ldhwaddell <lucas@hyperspark.ai>
2022-08-21 20:29:10 -0300

    romanToInt solution

--- easy/13.romanToInt.py
+++ easy/13.romanToInt.py
@@ -8,11 +8,25 @@ X can be placed before L (50) and C (100) to make 40 and 90.
 C can be placed before D (500) and M (1000) to make 400 and 900.
 Given a roman numeral, convert it to an integer.
 """
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        translations = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
+        }
+        number = 0
+        s = s.replace("IV", "IIII").replace("IX", "VIIII")
+        s = s.replace("XL", "XXXX").replace("XC", "LXXXX")
+        s = s.replace("CD", "CCCC").replace("CM", "DCCCC")
+        for char in s:
+            number += translations[char]
+        return number
 
-
-def romanToInt(s: str) -> int:
-    roman = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
-
-
-s = "III"
-romanToInt(s)
+sol = Solution()
+ans = sol.romanToInt("MXMMMM")
+print(ans)
