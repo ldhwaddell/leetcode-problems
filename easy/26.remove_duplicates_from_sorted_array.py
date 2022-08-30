b39a7b4680290991139febd0c373984327d9d515
ldhwaddell <lucas@hyperspark.ai>
2022-08-30 16:01:00 -0300

    solution to remove duplicates

--- easy/26.remove_duplicates_from_sorted_array.py
+++ easy/26.remove_duplicates_from_sorted_array.py
@@ -12,12 +12,16 @@ Do not allocate extra space for another array. You must do this by modifying the
 class Solution:
     def removeDuplicates(self, nums: list[int]) -> int:
         unique = 1
-        #for i in range(len(nums) - 1):
-
+        if len(nums) == 0:
+            return 0
+        for i in range(len(nums) - 1):
+            if nums[i] != nums[i+1]:
+                nums[unique] = nums[i+1]
+                unique += 1
         return unique
 
 
 sol = Solution()
-nums = [0, 0, 1, 2, 2, 3]
+nums = [0, 1, 1, 2, 2,3,3,3,3,3,3,3,3,3,9]
 ans = sol.removeDuplicates(nums)
 print(ans)
