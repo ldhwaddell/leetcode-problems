19ef97219f59707ab4dedb80ad4dcadce07971bb
ldhwaddell <ldhwaddell@gmail.com>
2022-09-01 19:04:09 -0300

    add new file

---
+++ easy/66.plus_one.py
@@ -0,0 +1,23 @@
+"""
+You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the integer. The digits are ordered from most significant to least significant in left-to-right order. The large integer does not contain any leading 0's.
+
+Increment the large integer by one and return the resulting array of digits.
+"""
+
+
+class Solution:
+    def plusOne(self, digits: list[int]) -> list[int]:
+        return 0
+
+
+sol = Solution()
+digits_1 = [4, 3, 2, 1]
+digits_2 = [9]
+ans = sol.lengthOfLastWord_v1(s)
+print(ans)
+
+#
+
+#
+
+# Time complexity:
