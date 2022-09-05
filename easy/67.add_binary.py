e6956184cc7cc01ed3441157513220267a33c25e
ldhwaddell <ldhwaddell@gmail.com>
2022-09-05 15:30:14 -0300

    first attempt at solution

--- easy/67.add_binary.py
+++ easy/67.add_binary.py
@@ -4,18 +4,32 @@ Given two binary strings a and b, return their sum as a binary string.
 
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        sum = 0
+        result = ""
+        carry = 0
 
-        return sum
+        # reverse input strings
+        a, b = a[::-1], b[::-1]
 
+        for i in range(max(len(a), len(b))):
+            digit_a = int(a[i]) if i < len(a) else 0
+            digit_b = int(b[i]) if i < len(b) else 0
 
+            total = digit_a + digit_b + carry
+            char = str(total % 2)
+            result = char + result
+            carry = total // 2
 
+        if carry:
+            result = "1" + result
+        
+        return result
 
 sol = Solution()
 a = "1010"
 b = "1011"
 ans = sol.addBinary(a, b)
 print(ans)
+
 
 # 
 
