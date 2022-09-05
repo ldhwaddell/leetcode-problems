1a575e8510a65db12075a7d12051f18bec04fdfb
ldhwaddell <ldhwaddell@gmail.com>
2022-09-05 14:51:50 -0300

    initial commit

---
+++ easy/67.add_binary.py
@@ -0,0 +1,24 @@
+"""
+Given two binary strings a and b, return their sum as a binary string.
+"""
+
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        sum = 0
+
+        return sum
+
+
+
+
+sol = Solution()
+a = "1010"
+b = "1011"
+ans = sol.addBinary(a, b)
+print(ans)
+
+# 
+
+# 
+
+# Time complexity:
