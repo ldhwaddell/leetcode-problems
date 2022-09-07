d733d2e403a12b890c25fdba5ded6e0604b45d75
ldhwaddell <ldhwaddell@gmail.com>
2022-09-06 22:28:06 -0300

    solution to remove dupes from sorted ll

--- easy/83.remove_duplicates_sorted_list.py
+++ easy/83.remove_duplicates_sorted_list.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 """
 Given the head of a sorted linked list, delete all duplicates such that each element appears only once. Return the linked list sorted as well.
 """
@@ -11,16 +13,27 @@ class ListNode:
 
 
 class Solution:
-    pass
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        # current node is head of list
+        cur = head
+        # while the current node has not been pointed to none
+        while cur:
+            # while the current node has a next value and the next nodes value equals the current nodes value
+            while cur.next and cur.next.val == cur.val:
+                # skip over it by assigning the next node to be the one after next
+                cur.next = cur.next.next
+            # other wise, advance the current node
+            cur = cur.next
+        # return the list
+        return head
 
 
 sol = Solution()
-n = 2
-ans = sol.climbStairs(n)
+ans = sol.deleteDuplicates()
 print(ans)
 
-# Runtime: 55 ms, faster than 26.27% of Python3 online submissions for Climbing Stairs.
+# Runtime: 77 ms, faster than 30.77% of Python3 online submissions for Remove Duplicates from Sorted List.
 
-# Memory Usage: 13.9 MB, less than 11.83% of Python3 online submissions for Climbing Stairs.
+# Memory Usage: 14 MB, less than 30.02% of Python3 online submissions for Remove Duplicates from Sorted List.
 
 # Time complexity:
