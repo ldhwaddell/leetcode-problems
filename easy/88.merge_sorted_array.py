f5ea9231234704e8e7d76f45a8834091f66cd793
ldhwaddell <ldhwaddell@gmail.com>
2022-09-11 22:19:19 -0300

    add solution

--- easy/88.merge_sorted_array.py
+++ easy/88.merge_sorted_array.py
@@ -9,19 +9,35 @@ The final sorted array should not be returned by the function, but instead be st
 
 class Solution:
     def merge(self, nums1: list[int], m: int, nums2: list[int], n: int) -> None:
-        nums1[0] = nums2[0] if m == 0 and n == 1 else nums1[0]
-        if m and n:
-            nums2_pointer = 0
-            for i in range(n):
-                if nums1[i] < nums2[i]:
-                    nums1.insert(i + 1, nums2[i])
+        # last index nums1
+        last = m + n - 1
 
-        print(nums1)
+        # merge in reverse order
+        while m > 0 and n > 0:
+            # if the last num1 val > last nums2 val
+            # put the great val at nums1[last]
+            if nums1[m - 1] > nums2[n - 1]:
+                nums1[last] = nums1[m - 1]
+                m -= 1
+
+            else:
+                nums1[last] = nums2[n - 1]
+                n -= 1
+
+            last -= 1
+
+        # fill the start of nums1 with the leftover elements from nums2
+        while n > 0:
+            nums1[last] = nums2[n - 1]
+            n -= 1
+            last -= 1
+
+        # print(nums1)
 
 
 sol = Solution()
 nums1 = [1, 2, 3, 0, 0, 0]
-m = 1
+m = 3
 nums2 = [2, 5, 6]
 n = 3
 sol.merge(nums1, m, nums2, n)
