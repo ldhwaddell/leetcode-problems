7795bf5084bf6f9bf5352fc9a9c867cf72099c31
ldhwaddell <ldhwaddell@gmail.com>
2022-09-19 21:24:22 -0300

    initial commit

---
+++ medium/3.longest_substring_without_repeating_chars.py
@@ -0,0 +1,21 @@
+"""
+Given a string s, find the length of the longest substring without repeating characters.
+"""
+
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        ...
+
+
+sol = Solution()
+s = "abcabcbb"
+ans = sol.lengthOfLongestSubstring(s)
+print(ans)
+
+
+#
+
+#
+
+# Time complexity:
