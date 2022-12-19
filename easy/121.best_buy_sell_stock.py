c6da8a61c46741ccc9a507fb7aa6bc6927b7597d
ldhwaddell <ldhwaddell@gmail.com>
2022-12-18 22:02:14 -0400

    add solution for best time to buy/sell stock

--- easy/121.best_buy_sell_stock.py
+++ easy/121.best_buy_sell_stock.py
@@ -9,11 +9,31 @@ Return the maximum profit you can achieve from this transaction. If you cannot a
 
 class Solution:
     def maxProfit(self, prices: list[int]) -> int:
-        ...
+        # 2 pointer method. Left pointer is index 0, right pointer is index 1
+        left, right = 0, 1
+        profit = 0
+
+        while right < len(prices):
+            # Get profit by subtracting right from left
+            curr_profit = prices[right] - prices[left]
+            # If the current profit is greater than max profit, swap it
+            if curr_profit > profit:
+                profit = curr_profit
+
+            # if the sell price is less than the current buy price,
+            # make the buy the new sell price and increment the sell position
+            if prices[right] < prices[left]:
+                left = right
+                right += 1
+            # If the sell price is not less than the buy price, keep incrementing the sell pointer
+            else:
+                right += 1
+
+        return profit
 
 
 sol = Solution()
-prices = [7,1,5,3,6,4]
+prices = [2, 1, 2, 1, 0, 1, 2]
 ans = sol.maxProfit(prices)
 print(ans)
 
