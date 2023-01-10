1eff0dc1af06b429ac58cdbce796d0a80d284a8d
ldhwaddell <ldhwaddell@gmail.com>
2023-01-09 22:31:06 -0400

    add BFS and DFS solutions for 200 # islands

--- leetcode_75/day_9_graph_bfs_dfs/200.number_of_islands.py
+++ leetcode_75/day_9_graph_bfs_dfs/200.number_of_islands.py
@@ -1,3 +1,6 @@
+from typing import List
+from collections import deque
+
 """
 Given an m x n 2D binary grid grid which represents a map of '1's (land) 
 and '0's (water), return the number of islands.
@@ -8,29 +11,102 @@ lands horizontally or vertically. You may assume all four edges of the grid are 
 
 
 class Solution:
-    def numIslands(self, grid: list[list[str]]) -> int:
-        rows, columns = len(grid), len(grid[0])
-        visited = [[False for _ in range(columns)] for _ in range(rows)]
+    def numIslands_bfs(self, grid: List[List[str]]) -> int:
+        if not grid:
+            return 0
 
-        count = 0
+        # Get dimensions of grid
+        rows, cols = len(grid), len(grid[0])
+        # Set to contained visited coords
+        visited = set()
+        islands = 0
 
-        for i in range(rows):
-            for j in range(columns):
-                ...
+        def bfs(r, c):
+            queue = deque()
+            # Add the coords to visited
+            visited.add((r, c))
+            # add the coords to queue
+            queue.append((r, c))
 
+            # Keep expanding queue while it is not empty
+            while queue:
+                row, col = queue.popleft()
+                # All possible next moves
+                directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+                # Check adjacent positions
+                for dr, dc in directions:
+                    r, c = row + dr, col + dc
+                    # Check that new position is valid
+                    if (
+                        r in range(rows)
+                        and c in range(cols)
+                        and grid[r][c] == "1"
+                        and (r, c) not in visited
+                    ):
+                        # If valid, add coord to queue and visited
+                        visited.add((r, c))
+                        queue.append((r, c))
 
-        return visited
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == "1" and (r, c) not in visited:
+                    # Run breadth first search on this coord
+                    bfs(r, c)
+                    islands += 1
+
+        return islands
+
+    def numIslands_dfs(self, grid: List[List[str]]) -> int:
+        if not grid:
+            return 0
+
+        # Get dimensions of grid
+        rows, cols = len(grid), len(grid[0])
+        # Set to contained visited coords
+        visited = set()
+        islands = 0
+
+        def dfs(r, c):
+            # Add coord to visited
+            visited.add((r, c))
+
+            directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+            # Check adjacent positions
+            for dr, dc in directions:
+                next_r, next_c = r + dr, c + dc
+                # Check that new position is valid
+                if (
+                    next_r in range(rows)
+                    and next_c in range(cols)
+                    and grid[next_r][next_c] == "1"
+                    and (next_r, next_c) not in visited
+                ):
+                    # If valid, add coord to queue and visited
+                    visited.add((next_r, next_c))
+                    dfs(next_r, next_c)
+
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == "1" and (r, c) not in visited:
+                    # Run depth first search on this coord
+                    dfs(r, c)
+                    islands += 1
+
+        return islands
 
 
 sol = Solution()
 grid = [
+    ["1", "1", "1", "1", "0"],
+    ["1", "1", "0", "1", "0"],
     ["1", "1", "0", "0", "0"],
-    ["1", "1", "0", "0", "0"],
-    ["0", "0", "1", "0", "0"],
-    ["0", "0", "0", "1", "1"],
+    ["0", "0", "0", "0", "0"],
 ]
-ans = sol.numIslands(grid)
-print(ans)
+ans_bfs = sol.numIslands_bfs(grid)
+ans_dfs = sol.numIslands_dfs(grid)
+
+print(f"BFS: {ans_bfs}, DFS: {ans_dfs}")
+
 
 #
 
