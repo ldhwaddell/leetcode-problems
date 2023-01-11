9435aa0c4b9a99f413b61071a4dbcd7421465f20
ldhwaddell <ldhwaddell@gmail.com>
2023-01-10 21:26:18 -0400

    test change

--- leetcode_75/day_12_2_pointer/438.find_all_anagrams_in_str.py
+++ leetcode_75/day_12_2_pointer/438.find_all_anagrams_in_str.py
@@ -1,5 +1,6 @@
 from typing import List
 from collections import Counter
+
 """
 Given two strings s and p, return an array of all the start indices of p's anagrams in s. 
 You may return the answer in any order.
@@ -15,23 +16,46 @@ class Solution:
             return []
 
         p_count, s_count = {}, {}
+        anagrams = []
+        remove = 0
 
-        # Create hashmap with all chars in p 
+        # Create hashmap with all chars in p
+        for c in p:
+            if c not in p_count:
+                p_count[c] = 1
+            else:
+                p_count[c] += 1
+
+        print(f"p count: {p_count}")
+
+        s_count[s[0]] = 1
+        s_count[s[1]] = 1 if s[0] != s[1] else 2
+
+        for i in range(2, len(s)):
+            # Add new letter to dict
+            if s[i] not in s_count:
+                s_count[s[i]] = 1
+            else:
+                s_count[s[i]] += 1
+
+            print(s_count)
 
 
-        
+            # Check for dict equality
+            if s_count == p_count:
+                anagrams.append(remove)
 
+            
 
+            s_count[s[remove]] -= 1
 
-        print(p_count, s_count, p_count==s_count)
-
-
-
+            remove += 1
+        return anagrams
 
 
 sol = Solution()
-s = "ababababab" 
-p = "aab"
+s = "abab"
+p = "ab"
 ans = sol.findAnagrams(s, p)
 print(ans)
 
